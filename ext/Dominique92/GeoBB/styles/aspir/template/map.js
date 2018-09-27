50589065e3187e27e55ee572d40444dd8a23d0ac
Dominique Cavailhez <protec@cavailhez.fr>
2018-09-27 18:37:36 +0200

    Refonte archi fichiers

---
+++ ext/Dominique92/GeoBB/styles/aspir/template/map.js
@@ -0,0 +1,3 @@
+/*DCMM*/{var _v='ASPIR MAP.JS',_r='';if(typeof _v=='array'||typeof _v=='object'){for(_i in _v)if(typeof _v[_i]!='function')_r+=_i+'='+typeof _v[_i]+' '+_v[_i]+' '+(_v[_i]&&_v[_i].CLASS_NAME?'('+_v[_i].CLASS_NAME+')':'')+"\n"}else _r+=_v;console.log(_r)}
+
+//TODO DELETE
