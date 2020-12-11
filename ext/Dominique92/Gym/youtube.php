bc7974803951d61467c24073fd5ca5aa91d5e997
Dominique Cavailhez <Dominique92@dc9.fr>
2020-12-11 14:55:16 +0100

    Bug GYM youtube

--- ext/Dominique92/Gym/youtube.php
+++ ext/Dominique92/Gym/youtube.php
@@ -2,12 +2,11 @@
 
 <?php
 file_put_contents ('../../../LOG/youtube.txt', var_export ([
-file_put_contents ('LOG/visio.LOG', var_export([
 	'date' => date(DATE_RFC2822),
-	'query' => $this->server['QUERY_STRING'],
-	'ip' => $this->server['REMOTE_ADDR'],
-	'referer' => @$this->server['HTTP_REFERER'],
-	'agent' => @$this->server['HTTP_USER_AGENT'],
+	'query' => $_SERVER['QUERY_STRING'],
+	'ip' => $_SERVER['REMOTE_ADDR'],
+	'referer' => @$_SERVER['HTTP_REFERER'],
+	'agent' => @$_SERVER['HTTP_USER_AGENT'],
 ], true), FILE_APPEND);
 
 echo '<meta http-equiv="refresh" content="0;URL=https://youtu.be/'.$_GET['y'].'">';
