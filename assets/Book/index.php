2a84991aefb824110a8cb1d251520b87c786e157
Dominique Cavailhez <Dominique92@dc9.fr>
2021-01-16 18:02:19 +0100

    Book single cover

--- assets/Book/index.php
+++ assets/Book/index.php
@@ -4,14 +4,57 @@
 	<meta charset="utf-8">
 	<title>Photos Dominique Cavailhez</title>
 	<link rel="stylesheet" href="book.css">
+
+	<style>
+		body: {
+		}
+		.rayon {
+			text-align: center;
+		}
+		.cover {
+			display: inline-block;
+			width: 32%;
+		}
+	</style>
 </head>
 
 <body>
-	<h1>TITRE</h1>
+	<div class="rayon">
+		<h1>RAYON</h1>
+		<div class="cover">
+			<div>
+				<h1>TITRE</h1>
+				<img src="http://chemineur.fr/download/file.php?id=787" />
+			</div>
+		</div>
+
+		<div class="cover">
+			<div>
+				<h1>TITRE</h1>
+				<img src="http://chemineur.fr/download/file.php?id=787" />
+			</div>
+		</div>
+
+		<div class="cover">
+			<div>
+				<h1>TITRE</h1>
+				<img src="http://chemineur.fr/download/file.php?id=787" />
+			</div>
+		</div>
+
+		<div class="cover">
+			<div>
+				<h1>TITRE</h1>
+				<img src="http://chemineur.fr/download/file.php?id=787" />
+			</div>
+		</div>
+	</div>
+
+	<h1>LECTURE</h1>
 
 	<div class="book">
 		<div>
-			<img src="http://www.refuges.info/photos_points/17436.jpeg?1376592365" />
+			<img src="http://www.refuges.info/photos_points/17436.jpeg" />
 		</div>
 	</div>
 
