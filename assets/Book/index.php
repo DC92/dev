9480bad74f169c7fd16795c67e20725f4e7a047b
Dominique Cavailhez <Dominique92@dc9.fr>
2021-01-16 08:25:56 +0100

    Book

---
+++ assets/Book/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Photos Dominique Cavailhez</title>
+	<link rel="stylesheet" href="book.css">
+</head>
+
+<body>
+	<h1>TITRE</h1>
+
+	<div class="book">
+		<div class="right">
+			<img src="http://www.refuges.info/photos_points/17436.jpeg?1376592365" />
+		</div>
+	</div>
+
+	<div class="book open">
+		<div class="left">
+			<p>Un petit texte</p>
+		</div>
+		<div class="right">
+			<img src="http://chemineur.fr/download/file.php?id=787" />
+		</div>
+	</div>
+</body>
+</html>
