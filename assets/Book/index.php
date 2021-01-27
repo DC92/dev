34fe8b737c65c957ceb93fdfe1d99102e392cfac
Dominique Cavailhez <Dominique92@dc9.fr>
2021-01-27 14:24:22 +0100

    Book entrée et un livre

--- assets/Book/index.php
+++ assets/Book/index.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" href="book.css">
 
 	<style>
+		/* Spécifique à cette page */
 		body: {
 		}
 		.rayon {
@@ -13,58 +14,79 @@
 		}
 		.cover {
 			display: inline-block;
-			width: 32%;
+			min-width: 32%;
 		}
 	</style>
 </head>
 
 <body>
+
+<?php
+foreach (glob('*/*') AS $f) {
+	preg_match ('/(.*)\/([0-9][0-9])([+-]).*\.(.)/', $f, $m);
+	if ($m) {
+		if ($m[4] == 'J')
+			$m[4] = 'j';
+		$books [$m[1]] [$m[2]] [$m[3]] [$m[4]] = $f;
+	}
+}
+
+$album_courant = @array_keys($_GET)[0];
+$page_courante = @$_GET[$album_courant];
+$file = @$books[$album_courant][$page_courante ?: '00'];
+
+function carre ($file, $side = '-') {
+	$r = '';
+	$h = 0; // Nb em de h1 & p
+	if (isset ($file[$side])) {
+		if (isset ($file[$side]['T'])) {
+			$r .= '<h1>'. @file_get_contents($file[$side]['T']) .'</h1>'.PHP_EOL;
+			$h += 2.3;
+		}
+		if (isset ($file[$side]['t'])) {
+			$r .= @file_get_contents($file[$side]['t']).PHP_EOL;
+			$h += 1.1;
+		}
+		if (isset ($file[$side]['j']))
+			$r .= "<div style='border:1px solid red;height:calc(100% - {$h}em)'>\n".
+				  "<img src='{$file[$side]['j']}' />\n</div>\n";
+	}
+	return $r;
+}
+
+// Entrée dans le site
+if (!$album_courant) { ?>
 	<div class="rayon">
-		<h1>RAYON</h1>
+<?php 
+	foreach ($books AS $album => $book) {
+		$titre = @file_get_contents ($book['00']['-t']);
+		?>
 		<div class="cover">
-			<div>
-				<h1>TITRE</h1>
-				<img src="http://chemineur.fr/download/file.php?id=787" />
-			</div>
+			<a href='?<?=$album?>' title='Ouvrir le livre'>
+				<?=carre($book['00'],'-')?>
+			</a>
 		</div>
-
-		<div class="cover">
-			<div>
-				<h1>TITRE</h1>
-				<img src="http://chemineur.fr/download/file.php?id=787" />
-			</div>
-		</div>
-
-		<div class="cover">
-			<div>
-				<h1>TITRE</h1>
-				<img src="http://chemineur.fr/download/file.php?id=787" />
-			</div>
-		</div>
-
-		<div class="cover">
-			<div>
-				<h1>TITRE</h1>
-				<img src="http://chemineur.fr/download/file.php?id=787" />
-			</div>
-		</div>
+<?php } ?>
 	</div>
-
-	<h1>LECTURE</h1>
-
+<?php 
+}
+// Un livre
+elseif (!$page_courante) { ?>
 	<div class="book">
 		<div>
-			<img src="http://www.refuges.info/photos_points/17436.jpeg" />
+			<?=carre($file,'-')?>
 		</div>
 	</div>
-
+<?php } else { ?>
 	<div class="book open">
 		<div class="left">
-			<p>Un petit texte</p>
+			<?=carre($file,'+')?>
 		</div>
 		<div>
-			<img src="http://chemineur.fr/download/file.php?id=787" />
+			<?=carre($file,'-')?>
 		</div>
 	</div>
+<?php } ?>
+
 </body>
 </html>
