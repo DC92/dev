643c67c6cf976f4a387879e45eb76b237611f570
Dominique Cavailhez <Dominique92@dc9.fr>
2021-03-19 20:48:04 +0100

    Passage style chemineur en extension

---
+++ ext/Dominique92/Chemineur/event/listener.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * EPGV specific functions & style for the phpBB Forum
+ *
+ * @copyright (c) 2020 Dominique Cavailhez
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ */
+
+namespace Dominique92\Chemineur\event;
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class listener implements EventSubscriberInterface
+{
+	// List of externals
+	public function __construct(
+		\phpbb\db\driver\driver_interface $db,
+		\phpbb\request\request_interface $request,
+		\phpbb\template\template $template,
+		\phpbb\user $user,
+		\phpbb\auth\auth $auth,
+		\phpbb\language\language $language
+	) {
+		$this->db = $db;
+		$this->request = $request;
+		$this->template = $template;
+		$this->user = $user;
+		$this->auth = $auth;
+		$this->language = $language;
+
+		$this->ns = explode ('\\', __NAMESPACE__);
+		$this->ext_path = 'ext/'.$this->ns[0].'/'.$this->ns[1].'/';
+/*
+		$this->cookies = $this->request->get_super_global(\phpbb\request\request_interface::COOKIE);
+		$this->args = $this->request->get_super_global(\phpbb\request\request_interface::REQUEST);
+		$this->server = $this->request->get_super_global(\phpbb\request\request_interface::SERVER);
+		$this->uri = $this->server['REQUEST_SCHEME'].'://'.$this->server['SERVER_NAME'].$this->server['REQUEST_URI'];
+*/
+	}
+
+	static public function getSubscribedEvents() {
+		return [
+			// All
+			'core.page_header' => 'page_header',
+		];
+	}
+
+	/**
+		ALL
+	*/
+	function page_header() {
+		// Includes language and style files of this extension
+//TODO		$this->language->add_lang ('common', $this->ns[0].'/'.$this->ns[1]);
+
+		// Includes style files of this extension
+if(0)//TODO
+		if (!strpos ($this->server['SCRIPT_NAME'], 'adm/'))
+			$this->template->set_style ([
+				$this->ext_path.'styles',
+				'styles', // core styles
+			]);
+
+		// Assign requested template
+		$this->template->assign_var ('EXT_PATH', $this->ext_path);
+	}
+}
