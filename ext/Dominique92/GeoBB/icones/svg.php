110a1f7495a6122c7185b48e2d9adc21d24a7470
Dominique Cavailhez <Dominique92@dc9.fr>
2021-03-22 10:23:30 +0100

    Redesign icone feu

--- ext/Dominique92/GeoBB/icones/svg.php
+++ ext/Dominique92/GeoBB/icones/svg.php
@@ -1,10 +1,44 @@
-<?php
+<?php /*
+Cet utilitaire produit toutes les icônes SVG utilisées par les cartes
+
+Syntaxe :
+	/images/icones/debut-du-nom-<element>_<element>_<element>.[svg|png]
+
+Chaque <element> désigne une forme élémentaire :
+	bus
+	cabane
+	camping
+	pointdeau (icône toute seule)
+	lac
+	parking
+	ravitaillement
+	sommet
+	triangle (pour passage délicat)
+
+ou une surcharge :
+	cle
+	eau (petite goutte d'eau)
+	feu (cheminée et fumée)
+	manqueunmur
+	x (une grande croix noire)
+
+ou une ou deux couleurs CSS :
+La première est la couleur de l'icône, la deuxième celle du toit et des murs
+	white_black (bâtiment à face blanche, contour et toits noirs)
+	blue (bâtiment bleu)
+	green (gîte)
+	red (refuge gardé)
+
+ou
+	a123.4.5 (caractère Ascii décimal &#123; à la position x = 4, y = 5)
+	t123 (taille de l'icône 123*123 - défaut 24)
+*/
+
 // Traduit le nom si nécéssaire
 include ('alias.php');
 $nom = isset ($alias[$_GET['nom']]) ? $alias[$_GET['nom']] : $_GET['nom'];
 
 // Recherche les éléments à afficher dans le nom du fichier
-// Suite de champs alphanumériques en minuscules, séparés par des _
 preg_match_all ('/([a-z]+)([0-9]*)\.?([0-9]*)\.?([0-9]*)_/', $nom.'_', $elements);
 
 // Elements par defaut
