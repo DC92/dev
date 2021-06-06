3cac7590d0daafc8bc1c38ce288c9cf808739146
Dominique Cavailhez <Dominique92@dc9.fr>
2021-06-06 21:45:58 +0200

    Test MyOl newvector (avec cluster sans étiquette)

--- assets/MyOl/examples/newvector.js
+++ assets/MyOl/examples/newvector.js
@@ -1,36 +1,62 @@
 // Vector layer
-var chemLayer = new ol.layer.Vector({
-	source: new ol.source.Vector({
-		format: new ol.format.GeoJSON(),
-		strategy: ol.loadingstrategy.bbox,
-		url: function(extent, resolution, projection) {
-			// Retreive checked parameters
-			/*			let list = permanentCheckboxList(options.selectorName).filter(
-								function(evt) {
-									return evt !== 'on'; // Except the "all" input (default value = "on")
-								}),*/
-			let bbox = null;
+const source = new ol.source.Vector({
+	format: new ol.format.GeoJSON(),
+	strategy: ol.loadingstrategy.bbox,
+	url: function(extent, resolution, projection) {
+		// Retreive checked parameters
+		/*			let list = permanentCheckboxList(options.selectorName).filter(
+							function(evt) {
+								return evt !== 'on'; // Except the "all" input (default value = "on")
+							}),*/
+		let bbox = null;
 
-			if (ol.extent.getWidth(extent) != Infinity) {
-				bbox = ol.proj.transformExtent(
-					extent,
-					projection.getCode(),
-					'EPSG:4326' // Received projection
-				);
-			}
-			return 'https://chemineur.fr/ext/Dominique92/GeoBB/gis.php?bbox=' + bbox.join(',');
-		},
-	}),
+		if (ol.extent.getWidth(extent) != Infinity) {
+			bbox = ol.proj.transformExtent(
+				extent,
+				projection.getCode(),
+				'EPSG:4326' // Received projection
+			);
+		}
+		//return 'https://chemineur.fr/ext/Dominique92/GeoBB/gis.php?bbox=' + bbox.join(',');
+		return 'http://www.refuges.info/api/bbox?nb_points=all&type_points=7,10,9,23,6,3,28&bbox=' + bbox.join(',');
+	},
+});
+
+const clusterSource = new ol.source.Cluster({
+	distance: 32,
+	source: source,
+});
+
+const chemLayer = new ol.layer.Vector({
+	source: clusterSource,
 
 	style: function(feature) {
 		const style = new ol.style.Style({
-				stroke: new ol.style.Stroke({ // Lines
-					color: 'blue',
-					width: 2,
+			wstroke: new ol.style.Stroke({ // Lines
+				color: 'blue',
+				width: 20,
+			}),
+			wfill: new ol.style.Fill({
+				color: '#3399CC',
+			}),
+
+			image: new ol.style.Circle({
+				radius: 16,
+				stroke: new ol.style.Stroke({
+					color: 'red',
 				}),
 			}),
-			icon = feature.getProperties().icon;
+		});
 
+		if (feature.getProperties().features.length == 1)
+			feature = feature.getProperties().features[0];
+
+		const typewri = feature.getProperties().type; // WRI
+		icon = feature.getProperties().icon; // Chemineur
+
+
+		if (typewri)
+			icon = 'http://www.refuges.info/images/icones/' + typewri.icone + '.svg';
 		if (icon)
 			style.setImage(new ol.style.Icon({
 				src: icon,
@@ -56,7 +82,7 @@ const hoverStyle = new ol.style.Style({
 	hoverLayer = new ol.layer.Vector({
 		source: new ol.source.Vector(),
 		style: function(feature) {
-			hoverStyle.getText().setText(feature.get('name'));
+			hoverStyle.getText().setText(feature.get('name') || feature.get('nom'));
 			return hoverStyle;
 		},
 	});
@@ -77,6 +103,7 @@ map.on('pointermove', function(evt) {
 		});
 
 	if (feature !== hoveredFeature) {
+		//TODO changer le curseur
 		if (hoveredFeature) {
 			hoverLayer.getSource().removeFeature(hoveredFeature);
 		}
