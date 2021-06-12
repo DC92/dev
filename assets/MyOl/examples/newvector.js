2cada52a603491f6f7a4c5155b282a36b334dc32
Dominique Cavailhez <Dominique92@dc9.fr>
2021-06-12 18:14:51 +0200

    MyOl newvector 2 level layers depending on zoom

--- assets/MyOl/examples/newvector.js
+++ assets/MyOl/examples/newvector.js
@@ -1,60 +1,5 @@
 // Vector layer
-function layerMassif() {
-	return layerJson({
-		urlBase: '//www.refuges.info/',
-		urlSuffix: 'api/polygones?type_polygon=1',
-
-		styleOptions: function(feature) {
-			return {
-				fill: new ol.style.Fill({
-					color: feature.get('couleur'),
-				}),
-				text: new ol.style.Text({
-					font: '14px Calibri,sans-serif',
-					backgroundStroke: new ol.style.Stroke({
-						color: 'black',
-					}),
-					backgroundFill: new ol.style.Stroke({
-						color: 'yellow',
-					}),
-					text: feature.get('nom'),
-					padding: [0, 3, 0, 3],
-				}),
-			};
-		},
-	});
-}
-
-function layerWRI() {
-	return layerJson({
-		urlBase: '//www.refuges.info/',
-		urlSuffix: 'api/bbox?nb_points=all&type_points=7,10,9,23,6,3,28&bbox=',
-		urlBbox: function(bbox) {
-			return bbox.join(',');
-		},
-		normalize: function(f) {
-			f.set('name', f.get('nom'));
-			f.set('link', f.get('lien'));
-			f.set('icon', '//www.refuges.info/images/icones/' + f.get('type').icone + '.svg');
-		},
-		clusterDistance: 32,
-	});
-}
-
-function layerChem() {
-	return layerJson({
-		urlBase: '//chemineur.fr/',
-		urlSuffix: 'ext/Dominique92/GeoBB/gis.php?bbox=',
-		urlBbox: function(bbox) {
-			return bbox.join(',');
-		},
-		clusterDistance: 32,
-	});
-}
-
 function layerJson(options) {
-	let pixelRatio = 0;
-
 	const source = new ol.source.Vector({
 			format: new ol.format.GeoJSON(),
 			strategy: options.urlBbox ? ol.loadingstrategy.bbox : ol.loadingstrategy.all,
@@ -65,19 +10,20 @@ function layerJson(options) {
 										return evt !== 'on'; // Except the "all" input (default value = "on")
 									}),*/
 
-				const bbox = ol.proj.transformExtent(
-					extent,
-					projection.getCode(),
-					'EPSG:4326' // Received projection
-				);
-
 				return options.urlBase + // url base that can varry (server name, ...)
 					options.urlSuffix + // url suffix to be defined separately from the urlBase
-					(options.urlBbox ? options.urlBbox(bbox) : '');
+					(!options.urlBbox ?
+						'' :
+						options.urlBbox(ol.proj.transformExtent(
+							extent,
+							projection.getCode(),
+							'EPSG:4326' // Received projection
+						)));
 			},
 		}),
 
-		clusterSource = !options.clusterDistance ? source :
+		clusterSource = !options.clusterDistance ?
+		source :
 		new ol.source.Cluster({
 			distance: options.clusterDistance,
 			source: source,
@@ -91,51 +37,53 @@ function layerJson(options) {
 			}
 		}),
 
-		layer = new ol.layer.Vector({
-			source: clusterSource,
-			style: function(feature) {
-				const style = new ol.style.Style(typeof options.styleOptions == 'function' ?
-					options.styleOptions(feature) :
-					options.styleOptions || {}
-				);
+		style = function(feature) {
+			const styleLoca = new ol.style.Style(typeof options.styleOptions == 'function' ?
+				options.styleOptions(feature) :
+				options.styleOptions || {}
+			);
 
-				if (feature.getProperties().features) {
-					if (feature.getProperties().features.length == 1)
-						// Only 1 feature in the cluster, display it
-						feature = feature.getProperties().features[0];
-					else {
-						// This is a cluster, display a circle with the number
-						style.setImage(new ol.style.Circle({
-							radius: 14,
-							stroke: new ol.style.Stroke({
-								color: 'blue',
-							}),
-							fill: new ol.style.Fill({
-								color: 'white',
-							}),
-						}));
-
-						style.setText(new ol.style.Text({
-							font: '14px Calibri,sans-serif',
-							stroke: new ol.style.Stroke({
-								color: 'blue',
-							}),
-							fill: new ol.style.Fill({
-								color: 'blue',
-							}),
-							text: feature.get('features').length.toString(),
-						}));
-					}
-				}
-
-				const icon = feature.get('icon'); //TODO mettre en option ???
-				if (icon)
-					style.setImage(new ol.style.Icon({
-						src: icon,
+			if (feature.getProperties().features) {
+				if (feature.getProperties().features.length == 1)
+					// Only 1 feature in the cluster, display it
+					feature = feature.getProperties().features[0];
+				else {
+					// This is a cluster, display a circle with the number
+					styleLoca.setImage(new ol.style.Circle({
+						radius: 14,
+						stroke: new ol.style.Stroke({
+							color: 'blue',
+						}),
+						fill: new ol.style.Fill({
+							color: 'white',
+						}),
 					}));
 
-				return style;
-			},
+					styleLoca.setText(new ol.style.Text({
+						font: '14px Calibri,sans-serif',
+						stroke: new ol.style.Stroke({
+							color: 'blue',
+						}),
+						fill: new ol.style.Fill({
+							color: 'blue',
+						}),
+						text: feature.get('features').length.toString(),
+					}));
+				}
+			}
+
+			const icon = feature.get('icon'); //TODO mettre en option ???
+			if (icon)
+				styleLoca.setImage(new ol.style.Icon({
+					src: icon,
+				}));
+
+			return styleLoca;
+		},
+
+		layer = new ol.layer.Vector({
+			source: clusterSource,
+			style: style,
 		});
 
 	// Normalize properties
@@ -146,16 +94,27 @@ function layerJson(options) {
 		});
 
 	// Tune the clustering distance following the zoom leval
+	let pixelRatio = 0;
+
 	layer.on('prerender', function(evt) {
 		// get the transform ratio from the layer frameState
 		const ratio = evt.frameState.pixelToCoordinateTransform[0];
 
-		if (pixelRatio != ratio) { // Only when changed
-			pixelRatio = ratio;
-
 			// Tune the clustering distance depending on the transform ratio
 			if (typeof clusterSource.setDistance == 'function')
 				clusterSource.setDistance(Math.max(8, Math.min(60, ratio)));
+
+		if (pixelRatio != ratio) { // Only when changed
+			pixelRatio = ratio;
+
+			// Switch to another layer above a zoom limit
+			if (ratio > options.pixelRatioMax) {
+				layer.setSource(options.layerAbove.getSource());
+				layer.setStyle(options.layerAbove.getStyle());
+			} else {
+				layer.setSource(clusterSource);
+				layer.setStyle(style);
+			}
 		}
 	});
 
@@ -163,7 +122,7 @@ function layerJson(options) {
 }
 
 /**
- * Control to display labels on hover a feature & execute click
+ * Control to display labels on hovering a feature & execute click
  * feature.properties.name : name to label the feature
  * feature.properties.link : link to call when click on the feature
  */
@@ -287,6 +246,55 @@ const map = new ol.Map({
 	}),
 });
 
-//map.addLayer(layerWRI());
-//map.addLayer(layerMassif());
-map.addLayer(layerChem());
+const layerMassif = layerJson({
+		urlBase: '//www.refuges.info/',
+		urlSuffix: 'api/polygones?type_polygon=1',
+
+		styleOptions: function(feature) {
+			return {
+				fill: new ol.style.Fill({
+					color: feature.get('couleur'),
+				}),
+				text: new ol.style.Text({
+					font: '14px Calibri,sans-serif',
+					backgroundStroke: new ol.style.Stroke({
+						color: 'black',
+					}),
+					backgroundFill: new ol.style.Stroke({
+						color: 'yellow',
+					}),
+					text: feature.get('nom'),
+					padding: [0, 3, 0, 3],
+				}),
+			};
+		},
+	}),
+
+	layerWRI = layerJson({
+		urlBase: '//www.refuges.info/',
+		urlSuffix: 'api/bbox?nb_points=all&type_points=7,10,9,23,6,3,28&bbox=',
+		urlBbox: function(bbox) {
+			return bbox.join(',');
+		},
+		normalize: function(f) {
+			f.set('name', f.get('nom'));
+			f.set('link', f.get('lien'));
+			f.set('icon', '//www.refuges.info/images/icones/' + f.get('type').icone + '.svg');
+		},
+		clusterDistance: 32,
+		pixelRatioMax: 150,
+		layerAbove: layerMassif,
+	}),
+
+	layerChem = layerJson({
+		urlBase: '//chemineur.fr/',
+		urlSuffix: 'ext/Dominique92/GeoBB/gis.php?bbox=',
+		urlBbox: function(bbox) {
+			return bbox.join(',');
+		},
+		clusterDistance: 32,
+	});
+
+map.addLayer(layerWRI);
+//map.addLayer(layerMassif);
+map.addLayer(layerChem);
