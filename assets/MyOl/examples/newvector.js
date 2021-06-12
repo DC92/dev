2270c40d55007b94abd8b47ddb16664da6203cb6
Dominique Cavailhez <Dominique92@dc9.fr>
2021-06-12 17:27:34 +0200

    MyOl newvector chem + wri + massifs OK

--- assets/MyOl/examples/newvector.js
+++ assets/MyOl/examples/newvector.js
@@ -1,19 +1,36 @@
 // Vector layer
-function layerChem() {
-	return layerVector({
-		urlBase: '//chemineur.fr/',
-		urlSuffix: function(bbox) {
-			return 'ext/Dominique92/GeoBB/gis.php?bbox=' + bbox.join(',');
+function layerMassif() {
+	return layerJson({
+		urlBase: '//www.refuges.info/',
+		urlSuffix: 'api/polygones?type_polygon=1',
+
+		styleOptions: function(feature) {
+			return {
+				fill: new ol.style.Fill({
+					color: feature.get('couleur'),
+				}),
+				text: new ol.style.Text({
+					font: '14px Calibri,sans-serif',
+					backgroundStroke: new ol.style.Stroke({
+						color: 'black',
+					}),
+					backgroundFill: new ol.style.Stroke({
+						color: 'yellow',
+					}),
+					text: feature.get('nom'),
+					padding: [0, 3, 0, 3],
+				}),
+			};
 		},
-		//		clusterDistance: 32,
 	});
 }
 
 function layerWRI() {
-	return layerVector({
+	return layerJson({
 		urlBase: '//www.refuges.info/',
-		urlSuffix: function(bbox) {
-			return 'api/bbox?nb_points=all&type_points=7,10,9,23,6,3,28&bbox=' + bbox.join(',');
+		urlSuffix: 'api/bbox?nb_points=all&type_points=7,10,9,23,6,3,28&bbox=',
+		urlBbox: function(bbox) {
+			return bbox.join(',');
 		},
 		normalize: function(f) {
 			f.set('name', f.get('nom'));
@@ -21,20 +38,26 @@ function layerWRI() {
 			f.set('icon', '//www.refuges.info/images/icones/' + f.get('type').icone + '.svg');
 		},
 		clusterDistance: 32,
-		altPixelRatio: 500,
-		altSource: new ol.source.Vector({
-			format: new ol.format.GeoJSON(),
-			url: 'https://www.refuges.info/api/polygones?type_polygon=1',
-		}),
 	});
 }
 
-function layerVector(options) {
+function layerChem() {
+	return layerJson({
+		urlBase: '//chemineur.fr/',
+		urlSuffix: 'ext/Dominique92/GeoBB/gis.php?bbox=',
+		urlBbox: function(bbox) {
+			return bbox.join(',');
+		},
+		clusterDistance: 32,
+	});
+}
+
+function layerJson(options) {
 	let pixelRatio = 0;
 
 	const source = new ol.source.Vector({
 			format: new ol.format.GeoJSON(),
-			strategy: ol.loadingstrategy.bbox,
+			strategy: options.urlBbox ? ol.loadingstrategy.bbox : ol.loadingstrategy.all,
 			url: function(extent, resolution, projection) {
 				//TODO Retreive checked parameters
 				/*			let list = permanentCheckboxList(options.selectorName).filter(
@@ -43,68 +66,69 @@ function layerVector(options) {
 									}),*/
 
 				const bbox = ol.proj.transformExtent(
-						extent,
-						projection.getCode(),
-						'EPSG:4326' // Received projection
-					),
-					// url suffix to be defined separately from the urlBase
-					urlSuffix =
-					typeof options.urlSuffix == 'function' ?
-					options.urlSuffix(bbox) :
-					options.urlSuffix || '';
+					extent,
+					projection.getCode(),
+					'EPSG:4326' // Received projection
+				);
 
-				return options.urlBase + urlSuffix;
+				return options.urlBase + // url base that can varry (server name, ...)
+					options.urlSuffix + // url suffix to be defined separately from the urlBase
+					(options.urlBbox ? options.urlBbox(bbox) : '');
 			},
 		}),
-		clusterSource = options.clusterDistance ? new ol.source.Cluster({
+
+		clusterSource = !options.clusterDistance ? source :
+		new ol.source.Cluster({
 			distance: options.clusterDistance,
 			source: source,
-			/*	geometryFunction: function(feature) {
+			geometryFunction: function(feature) {
+				if (feature.getGeometry().getGeometries)
 					//TODO getInteriorPoint() for polygons
-					return feature.getGeometry().getInteriorPoints().getPoint(0);
-					return feature.getGeometry();
-				}*/
-		}) : source,
+					//	return feature.getGeometry().getInteriorPoints().getPoint(0);
+					if (feature.getGeometry().getGeometries)
+						return;
+				return feature.getGeometry();
+			}
+		}),
+
 		layer = new ol.layer.Vector({
 			source: clusterSource,
 			style: function(feature) {
-				const style = new ol.style.Style({
-					image: new ol.style.Circle({
-						radius: 14,
-						stroke: new ol.style.Stroke({
-							color: 'blue',
-						}),
-						fill: new ol.style.Fill({
-							color: 'white',
-						}),
-					}),
-					/*//TODO declusteriser les lignes
-										stroke: new ol.style.Stroke({
-											color: 'blue',
-										}),
-										fill: new ol.style.Fill({
-											color: 'red',
-										}),
-										*/
-					text: new ol.style.Text({
-						font: '14px Calibri,sans-serif',
-						stroke: new ol.style.Stroke({
-							color: 'blue',
-						}),
-						fill: new ol.style.Fill({
-							color: 'blue',
-						}),
-					}),
-				});
+				const style = new ol.style.Style(typeof options.styleOptions == 'function' ?
+					options.styleOptions(feature) :
+					options.styleOptions || {}
+				);
 
 				if (feature.getProperties().features) {
 					if (feature.getProperties().features.length == 1)
+						// Only 1 feature in the cluster, display it
 						feature = feature.getProperties().features[0];
-					else
-						style.getText().setText(feature.get('features').length.toString());
+					else {
+						// This is a cluster, display a circle with the number
+						style.setImage(new ol.style.Circle({
+							radius: 14,
+							stroke: new ol.style.Stroke({
+								color: 'blue',
+							}),
+							fill: new ol.style.Fill({
+								color: 'white',
+							}),
+						}));
+
+						style.setText(new ol.style.Text({
+							font: '14px Calibri,sans-serif',
+							stroke: new ol.style.Stroke({
+								color: 'blue',
+							}),
+							fill: new ol.style.Fill({
+								color: 'blue',
+							}),
+							text: feature.get('features').length.toString(),
+						}));
+					}
 				}
 
-				const icon = feature.get('icon');
+				const icon = feature.get('icon'); //TODO mettre en option ???
 				if (icon)
 					style.setImage(new ol.style.Icon({
 						src: icon,
@@ -126,7 +150,7 @@ function layerVector(options) {
 		// get the transform ratio from the layer frameState
 		const ratio = evt.frameState.pixelToCoordinateTransform[0];
 
-		if (pixelRatio != ratio) {
+		if (pixelRatio != ratio) { // Only when changed
 			pixelRatio = ratio;
 
 			// Tune the clustering distance depending on the transform ratio
@@ -145,12 +169,9 @@ function layerVector(options) {
  */
 function controlHover() {
 	const control = new ol.control.Control({
-		element: document.createElement('div'), //HACK No button
-	});
-
-	let hoveredFeature;
-
-	const hoverStyle = new ol.style.Style({
+			element: document.createElement('div'), //HACK No button
+		}),
+		hoverStyle = new ol.style.Style({
 			text: new ol.style.Text({
 				font: '16px Calibri,sans-serif',
 				fill: new ol.style.Fill({
@@ -186,6 +207,7 @@ function controlHover() {
 			},
 		});
 
+	let hoveredFeature;
 	control.setMap = function(map) { //HACK execute actions on Map init
 		ol.control.Control.prototype.setMap.call(this, map);
 
@@ -219,7 +241,10 @@ function controlHover() {
 				return feature;
 			});
 
-			if (feature && feature.getProperties().features.length == 1)
+			if (feature &&
+				feature.getProperties().features &&
+				feature.getProperties().features.length == 1
+			)
 				feature = feature.getProperties().features[0];
 
 			if (feature) {
@@ -233,6 +258,7 @@ function controlHover() {
 						window.location = link;
 				}
 			}
+			//TODO zoom when click a cluster or a polygon (massifs)
 		});
 	};
 	return control;
@@ -255,11 +281,12 @@ const map = new ol.Map({
 		controlHover(),
 	],
 	view: new ol.View({
-		//		center: [700000, 5700000],// Maurienne
-		center: [260000, 6250000], // Paris
+		center: [700000, 5700000], // Maurienne
+		//center: [260000, 6250000], // Paris
 		zoom: 11,
 	}),
 });
 
 //map.addLayer(layerWRI());
+//map.addLayer(layerMassif());
 map.addLayer(layerChem());
