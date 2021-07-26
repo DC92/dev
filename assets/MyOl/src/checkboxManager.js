8217b41691a1e636488e65ed802045aad8bc295a
Dominique Cavailhez <Dominique92@dc9.fr>
2021-07-26 18:17:58 +0200

    checkboxManager updates

--- assets/MyOl/src/checkboxManager.js
+++ assets/MyOl/src/checkboxManager.js
@@ -7,38 +7,46 @@
  * Manages a global flip-flop of the same named <input> checkboxes
  */
 function memCheckbox(selectorName, callback) {
-	// Search values in cookies & args
-	const inputEls = document.getElementsByName(selectorName || ''),
-		request = window.location.search + ';' + // Priority to the url args ?selector=1,2,3
+	//TODO BUG ne mémorise que le callback de la dernière déclaration
+	const request = // Search values in cookies & args
+		window.location.search + ';' + // Priority to the url args ?selector=1,2,3
 		window.location.hash + ';' + // Then the hash #selector=1,2,3
 		document.cookie, // Then the cookies
-		match = request.match(new RegExp(selectorName + '=([^;]*)'));
+
+		match = request.match(new RegExp(selectorName + '=([^;]*)')),
+		inputEls = document.getElementsByName(selectorName || '');
 
 	if (inputEls)
 		for (let e = 0; e < inputEls.length; e++) { //HACK el.forEach is not supported by IE/Edge
-			// Check following cookies & args
+			// Set inputs following cookies & args
 			if (match)
 				inputEls[e].checked =
 				match[1].split(',').includes(inputEls[e].value) || // That one is declared
 				match[1].split(',').includes('on'); // The "all (= "on") is set
 
-			// Compute the all check && init the cookies if data has been given by the url
-			onClick({
-				target: inputEls[e],
-			});
-
 			// Attach the action
-			inputEls[e].addEventListener('click', onClick);
+			inputEls[e].onclick = onClick;
+
+			// Compute the all check && init the cookies if data has been given by the url
+			checkEl(inputEls[e]);
 		}
 
+	return readList(inputEls);
+
 	function onClick(evt) {
-		const list = []; // List of checked values
+		const list = checkEl(evt.target); // Do the "all" check verification
+
+		if (typeof callback == 'function')
+			callback(list);
+	}
+
+	function checkEl(target) {
 		let allIndex = -1, // Index of the "all" <input> if any
 			allCheck = true; // Are all others checked ?
 
 		for (let e = 0; e < inputEls.length; e++) {
-			if (evt.target.value == 'on') // If the "all" <input> is checked (who has a default value = "on")
-				inputEls[e].checked = evt.target.checked; // Force all the others to the same
+			if (target.value == 'on') // If the "all" <input> is checked (who has a default value = "on")
+				inputEls[e].checked = target.checked; // Force all the others to the same
 			else if (inputEls[e].value == 'on') // The "all" <input>
 				allIndex = e;
 			else if (!inputEls[e].checked)
@@ -50,18 +58,28 @@ function memCheckbox(selectorName, callback) {
 			inputEls[allIndex].checked = allCheck;
 
 		// Get the values of all checked inputs
-		for (let e = 0; e < inputEls.length; e++)
-			if (inputEls[e].checked &&
-				inputEls[e].value != 'on')
-				list.push(inputEls[e].value);
-
-		if (typeof callback == 'function')
-			callback(list);
+		const list = readList(inputEls);
 
 		// Mem the data in the cookie
 		if (selectorName)
 			document.cookie = selectorName + '=' + list.join(',') +
 			'; path=/; SameSite=Secure; expires=' +
 			new Date(2100, 0).toUTCString(); // Keep over all session
+
+		return list;
+	}
+
+	function readList(inputEls) {
+		if (inputEls.length == 1)
+			return [inputEls[0].checked];
+
+		const list = [];
+
+		for (let e = 0; e < inputEls.length; e++)
+			if (inputEls[e].checked &&
+				inputEls[e].value != 'on')
+				list.push(inputEls[e].value);
+
+		return list;
 	}
 }
