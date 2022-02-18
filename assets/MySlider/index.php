bff0c1bf358310d665f30bed4bc82a7f2088be6e
Dominique Cavailhez <Dominique92@dc9.fr>
2022-02-18 16:20:30 +0100

    MySlider.php automatique

---
+++ assets/MySlider/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Slider</title>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="MySlider.js"></script>
+	<link rel="stylesheet" href="MySlider.css">
+</head>
+
+<?php
+	$diapos = [];
+	foreach (glob('*.j*') AS $f)
+		$diapos[] = [$f, ''];
+?>
+
+<body>
+	<div id="myslider"></div>
+
+	<script>
+		mySlider (
+			'myslider', <?=json_encode($diapos)?>, {
+				showButtonsDelay: 1500, // Optional
+			}
+		);
+	</script>
+</body>
+</html>
