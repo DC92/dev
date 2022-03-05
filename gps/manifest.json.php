8d3ea19941b6eba41ab4f71983daa58c411e27ec
Dominique Cavailhez <Dominique92@dc9.fr>
2022-03-05 11:34:56 +0100

    New GPS

---
+++ gps/manifest.json.php
@@ -0,0 +1,11 @@
+{
+	"name": "GPS",
+	"display": "fullscreen",
+	"scope": "../",
+	"start_url": "index.php",
+	"icons": [{
+		"src": "../assets/MyOl/gps/favicon.png",
+		"sizes": "512x512",
+		"type": "image/png"
+	}]
+}
