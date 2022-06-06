e9365bc9b97d689f0bc8030383135bf823b76597
Dominique Cavailhez <Dominique92@dc9.fr>
2022-06-06 21:33:43 +0200

    GoeBB new features display / edit

--- assets/MyOl/src/marker.js
+++ assets/MyOl/src/marker.js
@@ -1,6 +1,11 @@
 /**
  * Marker position display & edit
  * Requires myol:onadd
+ * Options:
+ *   image : url of the marker image
+ *   prefix : id prefix of input/output values
+ *   focus : center & zoom on the marker
+ *   dragable : can draw the marker to edit position
  */
 function layerMarker(options) {
 	const els = [],
--- ext/Dominique92/GeoBB/styles/prosilver/template/layerGeoBB.js
+++ ext/Dominique92/GeoBB/styles/prosilver/template/layerGeoBB.js
@@ -1,55 +1,66 @@
 var map = new ol.Map({
-		target: 'map',
-		view: new ol.View({
-			enableRotation: false,
+	target: 'map',
+	view: new ol.View({
+		enableRotation: false,
+	}),
+
+	controls: controlsCollection({
+			permalink: {
+				display: mapType == 'index',
+			},
+		})
+		.concat(controlLayerSwitcher()),
+
+	layers: [
+		// Low map resolution : points
+		layerGeoBB({
+			host: '', // Relative adress
+			selectorName: 'geobb-features',
+			maxResolution: 100,
+			distance: 30,
+			attribution: 'Chemineur',
 		}),
-		controls: controlsCollection({
-				permalink: {
-					display: mapType == 'index',
-				},
-			})
-			.concat(controlLayerSwitcher()),
-		layers: [
-			// Low map resolution : points
-			layerGeoBB({
-				host: '', // Relative adress
-				selectorName: 'geobb-features',
-				maxResolution: 100,
-				distance: 30,
-				attribution: 'Chemineur',
-			}),
-			// High map resolution : clusters
-			layerGeoBB({
-				host: '', // Relative adress
-				subLayer: 'cluster',
-				selectorName: 'geobb-features',
-				minResolution: 100,
-				distance: 30,
-			}),
-		],
-	});
 
-if (scriptName == 'viewtopic')
-	map.addLayer(layerMarker('ext/Dominique92/GeoBB/styles/prosilver/theme/images/viewtopic.svg'));
+		// High map resolution : clusters
+		layerGeoBB({
+			host: '', // Relative adress
+			subLayer: 'cluster',
+			selectorName: 'geobb-features',
+			minResolution: 100,
+			distance: 30,
+		}),
 
-if (scriptName == 'posting' && mapType == 'point')
-	map.addLayer(layerMarker('ext/Dominique92/GeoBB/styles/prosilver/theme/images/posting.svg'), true);
+		mapType == 'point' ?
+		layerMarker({
+			image: 'ext/Dominique92/GeoBB/styles/prosilver/theme/images/' + scriptName + '.svg',
+			focus: 15,
+			dragable: scriptName == 'posting',
+		}) :
 
-if (scriptName == 'posting' && mapType == 'line')
-	map.addLayer(layerEditGeoJson({
-		geoJsonId: 'marker-json',
-		focus: 15,
-		titleModify: 'Modification d‘une ligne:\n' +
-			'Activer ce bouton (couleur jaune) puis\n' +
-			'Cliquer et déplacer un sommet pour modifier une ligne\n' +
-			'Cliquer sur un segment puis déplacer pour créer un sommet\n' +
-			'Alt+cliquer sur un sommet pour le supprimer\n' +
-			'Alt+cliquer sur un segment à supprimer dans une ligne pour la couper\n' +
-			'Joindre les extrémités deux lignes pour les fusionner\n' +
-			'Ctrl+Alt+cliquer sur une ligne pour la supprimer',
-		titleLine: 'Création d‘une ligne:\n' +
-			'Activer ce bouton (couleur jaune) puis\n' +
-			'Cliquer sur la carte et sur chaque point désiré pour dessiner une ligne,\n' +
-			'double cliquer pour terminer.\n' +
-			'Cliquer sur une extrémité d‘une ligne pour l‘étendre',
-	}));
+		// Line or polygon
+		layerEditGeoJson({
+			geoJsonId: 'marker-json',
+			focus: 15,
+			styleOptions: {
+				stroke: new ol.style.Stroke({
+					color: [0, 0, 128],
+					lineDash: [0, 20],
+					width: 8,
+				}),
+			},
+			titleModify: scriptName == 'viewtopic' ? '' : 'Modification d‘une ligne:\n' +
+				'Activer ce bouton (couleur jaune) puis\n' +
+				'Cliquer et déplacer un sommet pour modifier une ligne\n' +
+				'Cliquer sur un segment puis déplacer pour créer un sommet\n' +
+				'Alt+cliquer sur un sommet pour le supprimer\n' +
+				'Alt+cliquer sur un segment à supprimer dans une ligne pour la couper\n' +
+				'Joindre les extrémités deux lignes pour les fusionner\n' +
+				'Ctrl+Alt+cliquer sur une ligne pour la supprimer',
+			titleLine: scriptName == 'viewtopic' ? '' : 'Création d‘une ligne:\n' +
+				'Activer ce bouton (couleur jaune) puis\n' +
+				'Cliquer sur la carte et sur chaque point désiré pour dessiner une ligne,\n' +
+				'double cliquer pour terminer.\n' +
+				'Cliquer sur une extrémité d‘une ligne pour l‘étendre',
+		}),
+	],
+});
