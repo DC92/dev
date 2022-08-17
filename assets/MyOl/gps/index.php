520a105b0c2d85e417416d99be60ab7a18a8e890
Dominique Cavailhez <github@c92.fr>
2022-08-17 19:38:17 +0200

    Cosmétique

--- assets/MyOl/gps/index.php
+++ assets/MyOl/gps/index.php
@@ -94,7 +94,7 @@ Based on https://openlayers.org
 	<div id="gps-help">
 		<p>Pour utiliser les cartes et le GPS hors réseau:</p>
 		<hr /><p><u>Avant le départ:</u></p>
-		<p>- Explorateur -> options -> ajoutez à l&apos;écran d&apos;accueil (ou: installer)</p>
+		<p>- Explorateur -> options -> ajoutez à l'écran d'accueil (ou: installer)</p>
 		<p>Pour mémoriser un fond de carte:</p>
 		<p>- Choisissez un fond de carte</p>
 		<p>- Placez-vous au point de départ de votre randonnée</p>
@@ -102,7 +102,7 @@ Based on https://openlayers.org
 		<p>- Déplacez-vous suivant le trajet de votre randonnée suffisamment lentement pour charger toutes les dalles</p>
 		<p>- Recommencez avec les fonds de cartes que vous voulez mémoriser</p>
 		<p>* Toutes les dalles visualisées une fois seront conservées
-			dans le cache de l&apos;explorateur quelques jours et
+			dans le cache de l'explorateur quelques jours et
 			pourront être affichées même hors de portée du réseau</p>
 		<hr /><p><u>Hors réseau :</u></p>
 		<p>- Ouvrez le marque-page ou l'application</p>
@@ -111,8 +111,8 @@ Based on https://openlayers.org
 		<p>- Lancez la localisation en cliquant sur &#x2295;</p>
 		<hr />
 		<p>* Fonctionne bien sur Android avec Chrome, Edge, Brave, Samsung Internet, fonctions réduites avec Firefox & Safari</p>
-		<p>* Cette application ne permet pas de visualiser ou d&apos;enregistrer le parcours</p>
-		<p>* Aucune donnée ni géolocalisation n&apos;est remontée ni mémorisée</p>
+		<p>* Cette application ne permet pas de visualiser ou d'enregistrer le parcours</p>
+		<p>* Aucune donnée ni géolocalisation n'est remontée ni mémorisée</p>
 		<hr /><p>Mise à jour: <?=date('d M Y H:i:s')?> @<?=$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']?>
 		</p>
 	</div>
