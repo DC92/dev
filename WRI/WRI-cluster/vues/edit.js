e3a5d9743a288d5f94e8a23b44eead2acd5a53e7
Dominique Cavailhez <github@c92.fr>
2022-08-17 13:23:19 +0200

    Debug WRI edit massifs

---
+++ WRI/WRI-cluster/vues/edit.js
@@ -0,0 +1,84 @@
+const baseLayers = {
+		'Refuges.info': layerMRI(),
+		'OSM fr': layerOSM('//{a-c}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'),
+		'OpenTopo': layerOpenTopo(),
+		'Outdoors': layerThunderforest('outdoors'),
+		'IGN V2': layerIGN({
+			layer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2',
+			key: 'essentiels', // The key for the free layers
+			format: 'image/png',
+		}),
+		'Autriche': layerKompass('KOMPASS Touristik'),
+		'Espagne': layerSpain('mapa-raster', 'MTN'),
+		'Photo ArcGIS': layerArcGIS('World_Imagery'),
+		'Photo Bing': layerBing('Aerial'),
+	},
+
+  controls = [
+		new ol.control.Zoom(),
+		new ol.control.FullScreen(),
+		controlGeocoder(),
+		controlLoadGPX(),
+		//controlDownload(), //TODO
+		controlLayerSwitcher(baseLayers),
+		controlMousePosition(),
+		new ol.control.ScaleLine(),
+		controlPermalink({ // Permet de garder le même réglage de carte
+<?php if ($vue->polygone->id_polygone) { ?>
+			init: false, // Ici, on cadrera plutôt sur le massif
+<?php } ?>
+		}),
+		new ol.control.Attribution({
+			collapsed: false,
+		}),
+	],
+
+	// Affiche en noir la limite de tous les massifs
+	contours = layerVector({
+		url: '<?=$config_wri["sous_dossier_installation"]?>api/polygones?type_polygon=1',
+		style: new ol.style.Style({
+			stroke: new ol.style.Stroke({
+				color: 'blue',
+			}),
+		}),
+	}),
+
+	editeur = layerEditGeoJson({
+		geoJsonId: 'edit-json',
+		snapLayers: [contours],
+		help: [
+			document.getElementById('help-edit-modify').innerHTML,
+			null, // Pas d'édition de ligne
+			document.getElementById('help-edit-poly').innerHTML,
+		],
+		saveFeatures: function(coordinates, format) {
+			return format.writeGeometry(
+				new ol.geom.MultiPolygon(coordinates.polys),
+				{
+					featureProjection: 'EPSG:3857',
+					decimals: 5,
+				});
+		},
+	}),
+
+	map = new ol.Map({
+		target: 'carte-nav',
+		view: new ol.View({
+			enableRotation: false,
+		}),
+		controls: controls,
+		layers: [
+			contours,
+			editeur,
+		],
+	});
+
+	// Centrer sur la zone du polygone
+	<?if ($vue->polygone->id_polygone){?>
+		map.getView().fit(ol.proj.transformExtent([
+			<?=$vue->polygone->ouest?>,
+			<?=$vue->polygone->sud?>,
+			<?=$vue->polygone->est?>,
+			<?=$vue->polygone->nord?>,
+		], 'EPSG:4326', 'EPSG:3857'));
+	<?}?>
