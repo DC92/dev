8e134565a20320737f579445bbbacc2a50b08aad
Dominique Cavailhez <github@c92.fr>
2023-02-21 20:14:04 +0100

    npm myol suppression proj4 & geocoder

--- npm/build/index.js
+++ npm/build/index.js
@@ -3,15 +3,13 @@
  */
 
 import '../node_modules/ol/ol.css';
-import '../node_modules/@kirtandesai/ol-geocoder/dist/ol-geocoder.css';
-import '../node_modules/@kirtandesai/ol-geocoder/dist/ol-geocoder-debug.js';
-
-import '../src/controls.css';
-import '../src/editor.css';
-import '../src/layerSwitcher.css';
 
 export {default as View} from '../node_modules/ol/View.js';
 export {default as TileLayer } from '../node_modules/ol/layer/Tile.js';
 export {default as XYZ} from 'ol/source/XYZ';
+
+import '../src/controls.css';
+import '../src/editor.css';
+import '../src/layerSwitcher.css';
 
 export {default as MyMap} from '../src/MyMap.js';
