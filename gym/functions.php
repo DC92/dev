4677ecf860eee789b393a3e2471642461d3a9327
Dominique Cavailhez <github@c92.fr>
2023-04-29 22:00:04 +0200

    Gym bannière, menu & horaires

--- gym/functions.php
+++ gym/functions.php
@@ -3,12 +3,25 @@
 add_action("wp_enqueue_scripts", "theme_enqueue_styles");
 function theme_enqueue_styles()
 {
-    wp_enqueue_style(
-        "parent-style",
-        get_template_directory_uri() . "/style.css"
-    );
-   wp_register_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_style( 'style' );
+    wp_register_style("style", get_stylesheet_uri());
+    wp_enqueue_style("style");
+}
+
+// Use global urls in block templates (as defined in wp-includes/general-template.php)
+add_shortcode("get_info", "get_info_function");
+function get_info_function($args)
+{
+    return get_bloginfo($args[0]);
+}
+
+// Add favicon
+add_action("wp_head", "_block_template_favicon", 1);
+function _block_template_favicon()
+{
+    echo '<link rel="icon" type="image/jpg" href="' .
+        get_stylesheet_directory_uri() .
+        '/images/icon.jpg" />' .
+        PHP_EOL;
 }
 
 // Menu haut de page
@@ -17,33 +30,65 @@ function menu_function()
 {
     global $wpdb;
 
-    $results = $wpdb->get_results("
-SELECT parent.post_title AS parent_title, parent.post_name AS parent_name, child.*
+    preg_match('|/[^/]+/$|', $_SERVER["REQUEST_URI"], $page_url);
+
+    $pages = $wpdb->get_results("
+SELECT child.*,
+  parent.post_title AS parent_title,
+  parent.post_name AS parent_name, parent.ID AS parent_id
 FROM wpgym_posts AS parent
 JOIN wpgym_posts AS child ON parent.ID = child.post_parent
 WHERE parent.post_status = 'publish' AND child.post_status = 'publish'
 ORDER BY parent.menu_order, parent.post_title, child.menu_order, child.post_title
 ");
 
-    $r = ["<ul class='menu'>"];
+    $r = ['<ul class="menu">'];
 
-    foreach ($results as $res) {
-		// Au changement de ligne
-        if ($sous_menu != $res->post_parent) {
+    foreach ($pages as $p) {
+        // Au changement de ligne
+        if ($sous_menu != $p->post_parent) {
             if ($sous_menu) {
                 $r[] = "\t\t</ul>\n\t</li>";
             }
-            $r[] = "\t<li>\n\t\t<a href='/$res->parent_name/'>$res->parent_title</a>\n\t\t<ul>";
+            $r[] = "\t<li>\n\t\t<a href='/$p->parent_name/'>$p->parent_title</a>\n\t\t<ul>";
 
-            $sous_menu = $res->post_parent;
+            $sous_menu = $p->post_parent;
         }
-		
-		// Pour toutes les lignes
-        $r[] = "\t\t\t<li><a href='/$res->post_name/' title='Voir la page $res->post_title'>$res->post_title</a></li>";
+
+        // Pour toutes les lignes
+        $r[] = "\t\t\t<li><a href='/$p->post_name/' title='Voir la page $p->post_title'>$p->post_title</a></li>";
+
+        // Accueil
+        if (!count($page_url)) {
+            $id_edit = get_site_option("page_on_front");
+        }
+        // Catégorie
+        if ("/$p->parent_name/" == $page_url[0]) {
+            $id_edit = $p->parent_id;
+        }
+        // Entrée
+        if ("/$p->post_name/" == $page_url[0]) {
+            $id_edit = $p->ID;
+        }
     }
 
     $r[] = "\t\t</ul>\n\t</li>\n</ul>";
+    if ($id_edit) {
+        $r[] =
+            '<a class="crayon" title="Modification de la page" href="' .
+            get_bloginfo("url") .
+            "/wp-admin/post.php?&action=edit&post=" .
+            $id_edit .
+            '">&#9998;</a>';
+    }
+
     return implode(PHP_EOL, $r);
+}
+
+// Lien d'édition
+add_shortcode("edit_page", "edit_page_function");
+function edit_page_function()
+{
 }
 
 // Horaires
@@ -64,7 +109,7 @@ function horaires_function()
     preg_match('|/[^/]+/$|', $_SERVER["REQUEST_URI"], $page_url);
 
     $pages_publiees = $wpdb->get_results(
-        "SELECT * FROM wpgym_posts WHERE post_status = 'publish'"
+        'SELECT * FROM wpgym_posts WHERE post_status = "publish"'
     );
 
     foreach ($pages_publiees as $p) {
@@ -80,10 +125,19 @@ function horaires_function()
             $no_jour = array_search(strtolower(trim($date[0])), $jours_semaine);
 
             $ligne_horaire = [
-                "<a href=\"/$p->post_name/\" title=\"Voir l'activité\">{$colonnes[1][0]}</a>",
+                '<a title="Voir l\'activité" href="' .
+                get_bloginfo("url") .
+                "/{$p->post_name}/\">{$colonnes[1][0]}</a>",
+
                 $date[1],
-                "<a href=\"/{$post_names[$colonnes[1][2]]}/\" title=\"Voir le lieu\">{$colonnes[1][2]}</a>",
-                "<a href=\"/{$post_names[$colonnes[1][3]]}/\" title=\"Voir l'animateur-ice\">{$colonnes[1][3]}</a>",
+
+                '<a title="Voir le lieu" href="' .
+                get_bloginfo("url") .
+                "/{$post_names[$colonnes[1][2]]}/\">{$colonnes[1][2]}</a>",
+
+                '<a title="Voir l\'animateur-ice" href="' .
+                get_bloginfo("url") .
+                "/{$post_names[$colonnes[1][3]]}/\">{$colonnes[1][3]}</a>",
             ];
 
             if (
@@ -98,11 +152,14 @@ function horaires_function()
     }
 
     if (count($horaires)) {
-        $r[] = "<div class=\"horaires\">";
+        $r[] = "\n<div class=\"horaires\">";
 
         ksort($horaires);
         foreach ($horaires as $no_jour => $jour) {
-            $rj = ["\n\t<table>","\t<caption>{$jours_semaine[$no_jour]}</caption>\n\t"];
+            $rj = [
+                "\t<table>",
+                "\t\t<caption>{$jours_semaine[$no_jour]}</caption>",
+            ];
             ksort($jour);
             foreach ($jour as $heure) {
                 foreach ($heure as $ligne) {
