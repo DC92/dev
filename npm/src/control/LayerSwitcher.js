f12f42b8cd02df1c1e60e656d25173442f6d1426
Dominique Cavailhez <github@c92.fr>
2023-08-17 17:18:08 +0200

    npm refurbish layerswitcher

--- npm/src/control/LayerSwitcher.js
+++ npm/src/control/LayerSwitcher.js
@@ -5,35 +5,63 @@ import MyButton from './MyControl';
 import './layerSwitcher.css';
 
 //BEST how do we do on touch terminal ? alt key to switch layers / transparency
-//BEST keep open on click (as other buttons)
+//BEST keep selector open on click on the button (as other buttons)
 export default class LayerSwitcher extends MyButton {
 	constructor(options) {
 		super({
 			// MyButton options
 			className: 'myol-button-switcher',
 			label: '&#x274F;',
-			subMenuHTML: '<div id="myol-ls-range">' +
-				'<input type="range" title="Glisser pour faire varier la tranparence">' +
-				'<span>Ctrl+click: multicouches</span>' +
-				'</div>',
+			subMenuHTML: '<div>',
 
 			...options,
 		});
 
-		this.baseLayers = Object.fromEntries(
-			Object.entries(options.layers)
-			.filter(([_, v]) => v && !v.getProperties().hidden)
-		);
-		this.layerNames = Object.keys(this.baseLayers);
-		this.baselayer = location.href.match(/this.baselayer=([^\&]+)/);
+		// Filter null or hidden layers
+		this.layers = {};
+		for (let name in options.layers)
+			if (options.layers[name] && !options.layers[name].getProperties().hidden)
+				this.layers[name] = options.layers[name];
 
-		// Get baselayer from url if any
-		if (this.baselayer)
-			localStorage.myol_baselayer = decodeURI(this.baselayer[1]);
+		// Get baselayer from url hash (#baselayer=...) if any
+		const bl = location.href.match(/baselayer=([^\&]+)/);
+		if (bl)
+			localStorage.myol_baselayer = decodeURI(bl[1]);
+
+		this.sliderEl = document.createElement('input');
+		this.sliderEl.type = 'range';
+		this.sliderEl.title = 'Glisser pour faire varier la tranparence';
+		this.sliderEl.oninput = () => this.displayTransparencyRange();
 	}
 
 	setMap(map) {
 		super.setMap(map);
+
+		for (let name in this.layers) {
+			// Build html layers selectors
+			this.subMenuEl.insertAdjacentHTML('beforeend', '<label><input type="checkbox" name="baselayer" value="' + name + '" ' + ' />' + name + '</label>');
+
+			// Make layers available for display
+			this.layers[name].setVisible(false); // Don't begin to get the tiles yet (Necessary for Bing)
+			map.addLayer(this.layers[name]);
+		}
+		this.selectorEls = this.element.querySelectorAll('input[name="baselayer"]');
+		this.action(); // Display active layer
+		this.subMenuEl.insertAdjacentHTML('beforeend', '<p>Ctrl+click: multicouches</p>');
+
+		// Attach html additional selector (must be there to be after base layers)
+		const selectExtEl = document.getElementById(this.options.selectExtId);
+		if (selectExtEl) {
+			selectExtEl.classList.add('select-ext');
+			this.subMenuEl.appendChild(selectExtEl);
+			selectExtEl.style.display = ''; // Unmask the selector if it has been at the declaration
+		}
+
+		// Register action listeners
+		this.element.querySelectorAll('input')
+			.forEach(el => {
+				el.addEventListener('click', evt => this.action(evt));
+			});
 
 		// Hide the selector when the cursor is out of the selector
 		map.on('pointermove', evt => {
@@ -43,93 +71,42 @@ export default class LayerSwitcher extends MyButton {
 			if (evt.pixel[0] < max_x || evt.pixel[1] > max_y)
 				this.element.classList.remove('myol-button-switcher-open');
 		});
-
-		// Build html transparency slider
-		this.rangeContainerEl = document.getElementById('myol-ls-range');
-		this.rangeContainerEl.firstChild.oninput = () => this.displayTransparencyRange();
-
-		// Build html this.baseLayers selectors
-		for (let name in this.baseLayers) {
-			const labelEl = document.createElement('label');
-
-			labelEl.innerHTML = '<input type="checkbox" value="' + name + '" ' + ' />' + name;
-			labelEl.firstChild.onclick = evt => this.selectBaseLayer(evt); //BEST resorb all firstChild
-			this.subMenuEl.appendChild(labelEl);
-
-			this.baseLayers[name].setVisible(false); // Don't begin to get the tiles yet. Necessary for Bing
-			map.addLayer(this.baseLayers[name]);
-
-			// Mem it for further ops
-			this.baseLayers[name].inputEl = labelEl.firstChild; //BEST resorb
-		}
-
-		// Init layers
-		this.displayBaseLayers();
-
-		// Attach html additional selector
-		const selectExtEl = document.getElementById(this.options.selectExtId);
-
-		if (selectExtEl) {
-			selectExtEl.classList.add('select-ext');
-			this.subMenuEl.appendChild(selectExtEl);
-			// Unmask the selector if it has been at the declaration
-			selectExtEl.style.display = '';
-		}
 	}
 
-	selectBaseLayer(evt) {
-		// Single layer
-		if (!evt || !evt.ctrlKey || this.value == localStorage.myol_baselayer) {
-			this.transparentBaseLayerName = '';
-			localStorage.myol_baselayer = evt.target.value;
+	action(evt) {
+		// Clean checks
+		if (evt && !evt.ctrlKey) {
+			this.selectorEls.forEach(el => el.checked = false);
+			evt.target.checked = true;
 		}
-		// There is a second layer after the existing one
-		else if (this.layerNames.indexOf(localStorage.myol_baselayer) <
-			this.layerNames.indexOf(this.value)) {
-			this.transparentBaseLayerName = evt.target.value;
-			// localStorage.myol_baselayer don't change
-		}
-		// There is a second layer before the existing one
-		else {
-			this.transparentBaseLayerName = localStorage.myol_baselayer;
-			localStorage.myol_baselayer = evt.target.value;
-		}
+		if (!this.element.querySelector('input[name="baselayer"]:checked'))
+			(this.element.querySelector('input[value="' + localStorage.myol_baselayer + '"]') ||
+				this.selectorEls[0]
+			).checked = true;
 
-		this.rangeContainerEl.firstChild.value = 50;
-		this.displayBaseLayers();
-	}
+		const selectedEls = this.element.querySelectorAll('input[name="baselayer"]:checked');
 
-	displayBaseLayers() {
-		// Baselayer default is the first of the selection
-		if (!this.baseLayers[localStorage.myol_baselayer])
-			localStorage.myol_baselayer = this.layerNames[0];
+		localStorage.myol_baselayer = selectedEls[0].value;
+		this.sliderEl.value = 50;
+		this.sliderEl.remove();
+		this.transparentlayer = null;
 
-		for (let name in this.baseLayers) {
-			const visible =
-				name == localStorage.myol_baselayer ||
-				name == this.transparentBaseLayerName;
+		// Display the layers & slider
+		this.selectorEls.forEach(el => {
+			this.layers[el.value].setVisible(el.checked);
+			this.layers[el.value].setOpacity(1);
 
-			// Write the checks
-			this.baseLayers[name].inputEl.checked = visible;
-
-			// Make the right layers visible
-			this.baseLayers[name].setVisible(visible);
-			this.baseLayers[name].setOpacity(1);
-		}
+			if (el.checked && selectedEls.length > 1) {
+				el.parentElement.after(this.sliderEl);
+				this.transparentlayer = this.layers[el.value];
+			}
+		});
 
 		this.displayTransparencyRange();
 	}
 
-	displayTransparencyRange() { //TODO BUG don't work
-		if (this.transparentBaseLayerName) {
-			for (let l = 0; l < this.baseLayers[this.transparentBaseLayerName].length; l++)
-				this.baseLayers[this.transparentBaseLayerName][l].setOpacity(
-					this.rangeContainerEl.firstChild.value / 100
-				);
-
-			this.rangeContainerEl.className = 'myol-double-layer';
-		} else
-			this.rangeContainerEl.className = 'myol-single-layer';
+	displayTransparencyRange() {
+		if (this.transparentlayer)
+			this.transparentlayer.setOpacity(this.sliderEl.value / 100);
 	}
-
 }
