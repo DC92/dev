47e0b6f28524d07a34819fd1b9f509cf75eaa90a
Dominique Cavailhez <github@c92.fr>
2023-08-20 16:47:52 +0200

    npm gps 1st map

--- npm/gps/index.js
+++ npm/gps/index.js
@@ -1,96 +1,14 @@
-//TODO all GPS to be reviewed
-// Force https to allow PWA and geolocation
-// Force full script name of short url to allow PWA
-if (!location.href.match(/(https|localhost).*index/))
-	location.replace(
-		(location.hostname == 'localhost' ? 'http://' : 'https://') +
-		location.hostname +
-		location.pathname + (location.pathname.slice(-1) == '/' ? 'index.php' : '') +
-		location.search +
-		location.hash);
+/* global ol, myol */
 
-//TODO saving lint
-const ol = {};
-const myolPath = null;
-const swInstance = null;
-const buildDate = null;
-const gpxParam = null;
-const controlsCollection = null;
-const LayerSwitcher = null;
-const layerTileCollection = null;
-
-// Load service worker for web application, install & update
-if ('serviceWorker' in navigator)
-	navigator.serviceWorker.register(myolPath + 'service-worker.js.php?' + swInstance + buildDate, {
-		scope: './',
-	})
-	.then(registration => {
-		//BEST clarify this :
-		if (registration.installing)
-			console.log('PWA SW installing ' + registration.installing.scriptURL);
-		else if (registration.waiting)
-			console.log('PWA SW waiting ' + registration.waiting.scriptURL);
-		else if (registration.active)
-			console.log('PWA SW waiting ' + registration.active.scriptURL);
-		else
-			console.log('PWA SW STATUS UNKNOWN');
-
-		registration.onupdatefound = async function() { // service-worker.js is changed
-			console.log('PWA update found');
-
-			// Completely unregister the previous SW to avoid installed SW actions ongoing
-			if (registration.active) { // If it's an upgrade
-				await navigator.serviceWorker.getRegistrations().then(registrations => {
-					if (registrations.length) {
-						for (let reg of registrations)
-							//BEST need 3 reload to update
-							//BEST https://bitsofco.de/what-self-skipwaiting-does-to-the-service-worker-lifecycle/
-							//BEST https://stackoverflow.com/questions/59207110/in-a-pwa-is-there-a-way-to-check-for-a-waiting-service-worker-upgrade
-							reg.unregister()
-							.then(console.log('SW ' + reg.active.scriptURL + ' deleted'));
-					}
-				});
-			}
-
-			// Wait for end of all actions & reboot
-			const installingWorker = registration.installing;
-
-			if (installingWorker)
-				installingWorker.addEventListener('statechange', () => {
-					if (installingWorker.state === 'installed') {
-						console.log('PWA update installed / reload');
-						location.reload();
-					}
-				});
-		};
-	});
-
-// Manage the map
-var layers = [],
-	controlOptions = { // To be customized by the specific index.php
-		layerSwitcher: {},
-		LoadGPX: {
-			initFileUrl: gpxParam,
-		},
-		Help: {
-			subMenuId: 'myol-gps-help',
-		}
-	};
-
-window.addEventListener('load', function() {
-	if (!controlOptions.layerSwitcher.layers)
-		controlOptions.layerSwitcher.layers = layerTileCollection(controlOptions.layerSwitcher);
-
-	// Load the map
-	new ol.Map({
-		target: 'map',
-		view: new ol.View({
-			constrainResolution: true, // Force zoom on the available tile's definition
+new ol.Map({
+	target: 'map',
+	view: new ol.View({
+		enableRotation: false,
+	}),
+	controls: [
+		...myol.control.collection(),
+		new myol.control.LayerSwitcher({
+			layers: myol.layer.tile.collection(),
 		}),
-		controls: [
-			...controlsCollection(controlOptions),
-			new LayerSwitcher(controlOptions.layerSwitcher),
-		],
-		layers: layers,
-	});
+	],
 });
--- npm/gps/index.php
+++ npm/gps/index.php
@@ -1,48 +1,84 @@
 <?php
-header("Content-Type: text/html");  
+  header("Content-Type: text/html");
+  $gpx_files = [];
 ?><!DOCTYPE html>
+<!--
+© Dominique Cavailhez 2019
+https://github.com/Dominique92/MyOl
+Based on https://openlayers.org
+-->
 <html lang="en">
   <head>
     <!-- "Usual" title charset description viewport -->
-    <title>Barebones PWA</title>
+    <title>GPS PWA</title>
     <meta charset="utf-8" />
-    <meta name="description" content="A barebones PWA page" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.5" />
+    <meta name="description" content="An offline GPS based on Progressive Web Application" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
     <!-- Icons & platform specific -->
     <!-- Good old favicon -->
-    <link rel="icon" href="favicon.png" type="image/png" />
+    <link href="favicon.png" type="image/png" rel="icon" />
 
     <!-- Android / Chrome -->
     <meta name="mobile-web-app-capable" content="yes" />
     <meta name="theme-color" content="white" />
 
     <!-- IOS app icon + mobile Safari -->
-    <link rel="apple-touch-icon" href="icon-512.png" />
+    <link href="icon-512.png" rel="apple-touch-icon" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <meta name="apple-mobile-web-app-title" content="Hello World" />
 
-    <!-- WINDOWS -->
+    <!-- Windows -->
     <meta name="msapplication-TileImage" content="icon-512.png" />
     <meta name="msapplication-TileColor" content="#ffffff" />
 
     <!-- WEB APP manifest -->
-    <!-- https://web.dev/add-manifest/ -->
-    <link rel="manifest" href="manifest.json" />
+    <link href="manifest.json" rel="manifest" />
 
     <!-- Service worker -->
     <script>
-		if ("serviceWorker" in navigator)
-			navigator.serviceWorker.register("service-worker.js.php"); 
+    if ("serviceWorker" in navigator)
+      navigator.serviceWorker.register("service-worker.js.php"); 
     </script>
 
     <!-- App stylesheet + javascript -->
-	<link href="../dist/myol.css" rel="stylesheet">
-	<script src="../dist/myol.js"></script>
-	<script src="../examples/ressources/trace.js"></script>
+    <link href="../dist/myol.css" rel="stylesheet" />
+    <script src="../dist/myol.js"></script>
+    <link href="index.css" rel="stylesheet" />
+    <script src="index.js" defer></script>
+    <script src="../examples/ressources/trace.js"></script>
   </head>
-  <body class="select-ext">
-    It Works!
+  
+  <body>
+  <div id="map"></div>
+
+  <div id="myol-gps-help">
+    <p>Vous pouvez utiliser ce GPS hors réseau en l'installant:</p>
+    <hr /><p><u>Avant le départ:</u></p>
+    <p>- Explorateur -> options -> ajoutez à l'écran d'accueil (ou installer)</p>
+    <p>Pour mémoriser un fond de carte:</p>
+    <p>- Choisissez un fond de carte</p>
+    <p>- Placez-vous au point de départ de votre randonnée</p>
+    <p>- Zoomez au niveau le plus détaillé que vous voulez mémoriser</p>
+    <p>- Déplacez-vous suivant le trajet de votre randonnée suffisamment lentement pour charger toutes les dalles</p>
+    <p>- Recommencez avec les fonds de cartes que vous voulez mémoriser</p>
+    <p>* Toutes les dalles visualisées une fois seront conservées
+      dans le cache de l'explorateur quelques jours et
+      pourront être affichées même hors de portée du réseau</p>
+    <hr /><p><u>Hors réseau :</u></p>
+    <p>- Ouvrez le marque-page ou l'application</p>
+    <p>- Si vous avez un fichier trace .gpx dans votre mobile, visualisez-le en cliquant sur &#x1F4C2;</p>
+<?php if (count ($gpx_files) > 1) { ?>
+    <p>- Si vous voulez suivre une trace du serveur, affichez là en cliquant sur &#x1F6B6;</p>
+<?php } ?>
+    <p>- Lancez la localisation en cliquant sur &#x2295;</p>
+    <hr />
+    <p>* Fonctionne bien sur Android avec Chrome, Edge, Brave, Samsung Internet,
+      fonctions réduites avec Firefox & Safari</p>
+    <p>* Cette application ne permet pas de visualiser ni d'enregistrer le parcours</p>
+    <p>* Aucune donnée ni géolocalisation n'est remontée ni mémorisée</p>
+    <hr />
+  </div>
   </body>
 </html>
