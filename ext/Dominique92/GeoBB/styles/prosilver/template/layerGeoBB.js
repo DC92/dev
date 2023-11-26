1afaf8a5f3288432cac6affc4de816ae9d6d159c
Dominique Cavailhez <dominique@c92.fr>
2023-11-26 17:35:31 +0100

    Quelques bugs chemineur

--- ext/Dominique92/GeoBB/styles/prosilver/template/layerGeoBB.js
+++ ext/Dominique92/GeoBB/styles/prosilver/template/layerGeoBB.js
@@ -1,43 +1,54 @@
 myol.trace();
 
-var map = new ol.Map({
-	target: 'map',
-	view: new ol.View({
-		enableRotation: false,
+var geobbLayer = new myol.layer.vector.GeoBB({
+		selectName: 'select-geobb',
+		host: '', // Relative to this location
+		noClick: scriptName == 'posting',
+		noHover: scriptName == 'posting',
 	}),
-	controls: [
-		...myol.control.collection(),
-		new myol.control.LayerSwitcher({
-			layers: myol.layer.tile.collection(mapKeys),
-			selectExtId: 'select-ext',
-		}),
-		new myol.control.Permalink({
-			init: mapType != 'line' || scriptName != 'viewtopic',
-			display: scriptName == 'index',
-		}),
-	],
-	layers: [
-		new myol.layer.vector.GeoBB({
-			selectName: 'select-geobb',
-			host: '', // Relative to this location
-			noClick: scriptName == 'posting',
-			noHover: scriptName == 'posting',
-			/*
-			urlParams: { //BEST implement ???
-				v: version, // Reload layer if posting called between
-			},
-			*/
-		}),
-		new myol.layer.Hover(),
-	],
-});
 
-if (mapType == 'point')
-	map.addLayer(new myol.layer.Marker({
+	markerLayer = new myol.layer.Marker({
 		src: 'ext/Dominique92/GeoBB/styles/prosilver/theme/images/' + scriptName + '.svg',
 		focus: 15, // Map zoom level
 		dragable: scriptName == 'posting',
-	}));
+	}),
+
+	editorLayer = new myol.layer.Editor({
+		geoJsonId: 'marker-json',
+		editOnly: 'line',
+	}),
+
+	map = new ol.Map({
+		target: 'map',
+		view: new ol.View({
+			enableRotation: false,
+		}),
+		controls: [
+			...myol.control.collection({
+				download: {
+					savedLayer: editorLayer,
+				}
+			}),
+			new myol.control.LayerSwitcher({
+				layers: myol.layer.tile.collection(mapKeys),
+				selectExtId: 'select-ext',
+			}),
+			new myol.control.Permalink({
+				init: mapType != 'point' && (mapType != 'line' || scriptName != 'viewtopic'),
+				display: scriptName == 'index',
+			}),
+		],
+		layers: [
+			geobbLayer,
+			new myol.layer.Hover(),
+		],
+	});
+
+if (mapType == 'point')
+	map.addLayer(markerLayer);
+
+if (mapType == 'line' && scriptName == 'posting')
+	map.addLayer(editorLayer);
 
 if (mapType == 'line' && scriptName == 'viewtopic') {
 	const geoJson = document.getElementById('marker-json'),
@@ -53,10 +64,3 @@ if (mapType == 'line' && scriptName == 'viewtopic') {
 		maxZoom: 15,
 	});
 }
-
-if (mapType == 'line' && scriptName == 'posting')
-	//BEST save only layerEditGeoJson.layer
-	map.addLayer(new myol.layer.Editor({
-		geoJsonId: 'marker-json',
-		editOnly: 'line',
-	}));
